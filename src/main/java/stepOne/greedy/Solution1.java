d1aa991fd706de67400d1e5746ac5f4885e18580
bkdoo <kyeongdoos@gmail.com>
2022-08-19 02:37:47 +0900

    20220819 여벌 체육복을 이용하여 체육 수업을 진행할 수 있는 학생 수를 구하는 문제 정답해결

--- src/main/java/stepOne/greedy/Solution1.java
+++ src/main/java/stepOne/greedy/Solution1.java
@@ -1,67 +1,56 @@
 package stepOne.greedy;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution1 {
-    public <A> int solution(int n, int[] lost, int[] reserve) {
+    public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 1; i <=n; i++) {
             map.put(i,1);
         }
-        Stack<Integer> lostStack = new Stack<>();
-        Stack<Integer> reserveStack = new Stack<>();
-
-
         for (int i = 0; i < reserve.length; i++) {
             map.put(reserve[i], 2);
         }
-
         for (int i = 0; i < lost.length; i++) {
-            lostStack.push(lost[i]);
             map.put(lost[i], map.get(lost[i])-1);
         }
-
-        for (Integer i: map.keySet()
-             ) {
-            if(map.get(i)<2) {
-                map.remove(i);
+        int[] arr = new int[n];
+        for(Integer i : map.keySet()) {
+            int key = i;
+            int value = map.get(i);
+            arr[key-1] = value;
+        }
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]==2 && arr[i+1]==0) {
+                arr[i]=1;
+                arr[i+1]=1;
+            } else if(arr[i]==0 && arr[i+1]==2) {
+                arr[i]=1;
+                arr[i+1]=1;
             }
-
         }
 
-        List<Integer> list = map.keySet().stream().toList();
-        list.stream().sorted();
-
-        for (int i = 0; i < list.size(); i++) {
-            reserveStack.push(list.get(i));
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>0) {
+                answer++;
+            }
         }
 
-        while (!reserveStack.isEmpty()) {
-            int temp = reserveStack.pop();
-            if(lostStack.peek()+1 == temp || lostStack.peek()-1 == temp) {
-                lostStack.pop();
-            }
 
-            if(lostStack.isEmpty()) {
-                break;
-            }
 
-        }
-        answer = n - lostStack.size();
+
         return answer;
     }
 
     public static void main(String[] args) {
-        int[] lost = {3};
-        int[] reserve = {1};
+        int[] lost = {3,2};
+        int[] reserve = {1,4};
 
         Solution1 solution1 = new Solution1();
 
-        int result = solution1.solution(3, lost, reserve);
+        int result = solution1.solution(4, lost, reserve);
         System.out.println("result = " + result);
     }
 }
